b79aed60ee3c8760feba4ccfd5b6e512a57280a3
Tom France <tfranceuk@gmail.com>
2015-12-13 15:13:48 +0000

    added single room types view for mobile, with slide transition

--- content/app.js
+++ content/app.js
@@ -1,19 +1,18 @@
 (function() {
-  $(function() {
+  function buildTemplates() {
     var roomTypes = JSON.parse($('script[type="application/json"]#friends-json').text());
-    console.log(roomTypes);
-    var template = $('script[type="text/template"]#room-type').html();
-    console.log(template);
-    var container = $('#room-types-container-all');
-
+    var templateString = $('script[type="text/template"]#room-type').html();
+    var templates = [];
     for (var k in roomTypes) {
       var friends = roomTypes[k].friends.sort();
 
-      var bound = template
+      var bound = templateString
         .replace('{{name}}', k)
         .replace('{{friends}}', getFriendsString(friends));
-      $(bound).appendTo(container);
+      templates.push({name: k, template: $(bound)});
     }
+
+    return templates;
 
     function getFriendsString(friends) {
       switch (friends.length) {
@@ -24,6 +23,42 @@
         case 4: return `${friends[0]}, ${friends[1]}, and ${friends.length-2} other friends have stayed here`;
       }
     }
+  }
+
+  function appendTemplatesToContainerAll(templates) {
+    var container = $('#room-types-container-all');
+    templates.forEach(x => container.append(x.template.clone()));
+  }
+
+
+  $(function() {
+    var templates = buildTemplates();
+    appendTemplatesToContainerAll(templates);
+
+    //refactor into function
+    var select = $('#room-types-select');
+    var i = 0;
+    templates.forEach(x => {
+      var option = $('<option>').attr('value', i++).text(x.name).appendTo(select);
+    });
+    $('option:first', select).attr('selected', 'selected');
+    var current = templates[0].template.clone();
+    select.change(function(e) {
+      var next = templates[e.target.value*1].template.clone();
+
+      current.on('transitionend', function() {
+        console.log('transitionend');
+        this.remove();
+      });
+      next.addClass('in active');
+      setTimeout(() => next.removeClass('in'), 0);
+      current.addClass('out');
+      container.append(next);
+      current = next;
+    });
+    var container = $('#room-types-container-one');
+    current.addClass('active');
+    container.append(current);
   });
 })();
 
